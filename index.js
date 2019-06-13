540b432571c45a59de4903d44f87a7f166ea69cc
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-06-13 17:35:25 -0400

    ugh

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function hideModal(){
 }
 
 function getSentenceTextElement(sentenceId) {
-  return document.getElementById(`sentence-${ sentence.id }-text`)
+  return document.getElementById(`sentence-${ sentenceId }-text`)
 }
 
 function slapSentenceUpdateOnTheDom(sentence){
